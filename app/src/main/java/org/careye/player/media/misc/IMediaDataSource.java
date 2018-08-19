3207f9f57cf10530138ebc5dce9073996231fefc
Zhiqiang <panzhiqiang@momenta.ai>
2018-08-20 00:48:44 +0800

    更新代码结构

--- app/src/main/java/org/careye/player/media/misc/IMediaDataSource.java
+++ app/src/main/java/org/careye/player/media/misc/IMediaDataSource.java
@@ -1,6 +1,7 @@
 /*
- * Copyright (C) 2015 Bilibili
- * Copyright (C) 2015 Zhang Rui <bbcallen@gmail.com>
+ * Car eye 车辆管理平台: www.car-eye.cn
+ * Car eye 开源网址: https://github.com/Car-eye-team
+ * Copyright 2018
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- app/src/main/java/org/careye/util/PicUtils.java
+++ app/src/main/java/org/careye/util/PicUtils.java
@@ -1,3 +1,8 @@
+/*
+ * Car eye 车辆管理平台: www.car-eye.cn
+ * Car eye 开源网址: https://github.com/Car-eye-team
+ * Copyright 2018
+ */
 package org.careye.util;
 
 import android.graphics.Bitmap;
